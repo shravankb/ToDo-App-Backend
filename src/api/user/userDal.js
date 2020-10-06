c8b80f9fdf1a8253cd27768015658607beff8a0b
Shaguntiwari22 <shagun.tiwari@nineleaps.com>
2020-10-06 16:00:19 +0530

    signIn api done

--- src/api/user/userDal.js
+++ src/api/user/userDal.js
@@ -1,1 +1,12 @@
-console.log();
+
+const userModel = require('./userModel');
+
+createUser = async(userInfo)=>{
+try {
+   return  await  userModel.create(userInfo) 
+} catch (error) {
+    throw error
+}
+}
+
+module.exports ={ createUser}
--- src/api/user/userServices.js
+++ src/api/user/userServices.js
@@ -1,6 +1,20 @@
-signIn = async(token)=>{
+const userDal = require('./userDal');
+
+signIn = async(body)=>{
     try {
-        return token;
+        console.log(body)
+        let googleToken = body.googleToken;
+            let googleData = googleToken.split('.');
+            let decodedData = Buffer.from(googleData[1],'base64').toString('ascii');
+            console.log(decodedData)
+            let user={
+                email: decodedData.email,
+                name: decodedData.name
+            }
+            user = await userDal.createUser(user);
+
+        return user;
+
 
     } catch (error) {
         throw error;
